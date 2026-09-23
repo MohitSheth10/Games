2b36d1c30f47b02ec06ec1e3ce8846c4aa9eb9b2
Mohit Sheth <magicpotter2009@gmail.com>
2026-09-23 22:29:47 +0530

    Validate range inputs for random guessing game
    Added validation to ensure the end of the range is greater than the start.

--- random_guessing_game.py
+++ random_guessing_game.py
@@ -4,8 +4,8 @@ while bottom<0:
   print("Enter a positive number: ")
   bottom=int(input("Enter the start of the range:\n"))
 top=int(input("Enter end of the range:\n"))
-while bottom<0:
-  print("Enter a positive number:\n")
+while top<bottom:
+  print("The end of the range must be bigger than the start.")
   top=int(input("Enter end of the range:\n"))
 num2=random.randint(bottom,top)
 num=0
